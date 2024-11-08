8933ea21587502cd913dbca1b41db387e9384ef8
Alex <alxxander.nguyen@protonmail.com>
2024-11-08 14:50:28 -0800

    fixed morphology check

--- phenoscope/interface/_morphology_morpher.py
+++ phenoscope/interface/_morphology_morpher.py
@@ -10,15 +10,16 @@ from ..util.error_message import INTERFACE_ERROR_MSG, ENHANCED_ARRAY_CHANGE_ERRO
 class MorphologyMorpher(ImageOperation):
     def morph(self, image: Image, inplace: bool = False) -> Image:
         if image.object_mask is None: raise ValueError(MISSING_MASK_ERROR_MSG)
-        input_image: Image = image.copy()
+        input_arr = image.array
+        input_enhanced_arr = image.enhanced_array
 
         if inplace:
             output = self._operate(image)
         else:
             output = self._operate(image.copy())
 
-        if not np.array_equal(input_image.array, output.array): raise AttributeError(ARRAY_CHANGE_ERROR_MSG)
-        if not np.array_equal(input_image.enhanced_array, output.enhanced_array): raise AttributeError(ENHANCED_ARRAY_CHANGE_ERROR_MSG)
+        if not np.array_equal(input_arr, output.array): raise AttributeError(ARRAY_CHANGE_ERROR_MSG)
+        if not np.array_equal(input_enhanced_arr, output.enhanced_array): raise AttributeError(ENHANCED_ARRAY_CHANGE_ERROR_MSG)
 
         return output
 
