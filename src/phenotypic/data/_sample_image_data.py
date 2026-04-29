054e46950676439b053051d329cd4bc740649e10
Alexander Nguyen <alxxander.nguyen@protonmail.com>
2026-04-29 11:22:16 -0700

    Fix: fixed yeast sample plate having the wrong number of columns

--- src/phenotypic/data/_sample_image_data.py
+++ src/phenotypic/data/_sample_image_data.py
@@ -182,7 +182,7 @@ def load_yeast_plate(
                                  "RhodotorulaYeastCenterCrop.png",
             mode=mode,
             nrows=2,
-            ncols=4
+            ncols=3
     )
 
 
